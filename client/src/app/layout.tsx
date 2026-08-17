c62dc9c894c483ec9c93a3885503a2e372560ea8
andrew <akariaka@yandex.ru>
2026-08-18 03:12:11 +0600

    begin session

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +25,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="bg-[#253d48]">
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,18 @@
-import Link from 'next/link'
+'use client'
+// import Link from 'next/link'
+import { useSession } from 'next-auth/react'
+// import { ReactNode } from 'react';
 
 export default function Home() {
+  const session = useSession();
+ 
+  console.log(session);
+  
   return (
     <main className="font-sans flex w-full h-[100vh] flex-col items-center justify-center py-32 px-16">
       <div className='flex justify-center gap-[20px] text-[white]'>
-        <Link className='text-[30px]' href='signin'>Вход</Link>
-        <Link className='text-[30px]' href='signup'>Регистрация</Link>
+        {/* <Link className='text-[30px]' href='signin'>Вход</Link>
+        <Link className='text-[30px]' href='signup'>Регистрация</Link> */}
       </div>
     </main>
   );
