fe3621c19f18bef5ad564791584abd7248e5a8c1
andrew <akariaka@yandex.ru>
2026-08-19 15:39:01 +0600

    edit readme, create proxy.ts

--- client/src/app/utils/supabase/server.ts
+++ client/src/app/utils/supabase/server.ts
@@ -16,10 +16,8 @@ const createClient = (cookieStore: Awaited<ReturnType<typeof cookies>>) => {
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+          } catch(error) {
+            console.log(error);
           }
         },
       },
---
+++ client/src/proxy.ts
@@ -0,0 +1,7 @@
+import { type NextRequest, NextResponse } from "next/server";
+
+export async function proxy(request: NextRequest) {
+  const response = NextResponse.next({
+    
+  })
+}
