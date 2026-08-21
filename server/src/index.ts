efd6459d4bed47f6d9dd6b143710ca538533125a
akariaka@yandex.ru <akariaka@yandex.ru>
2026-08-21 22:00:19 +0600

    create config sqlite db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,7 @@
 import express, { type Request, type Response } from "express";
+import configDb from "./config/db.ts";
+
+const dbRun = configDb();
 
 const app = express();
 const port = 3000;
@@ -6,6 +9,10 @@ const port = 3000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello TypeScript + Express!");
 });
+
+app.post('/auth/signup', (req, res) => {
+
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
