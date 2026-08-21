13a4cc9e5320b8a934c837e42e61c79c0b190f74
akariaka@yandex.ru <akariaka@yandex.ru>
2026-08-21 23:12:57 +0600

    post req auth/signpu working; test postman

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,21 +1,19 @@
 import sqlite3 from "sqlite3";
 import path from "path";
 
-const configDb = () => {
-  const db = new sqlite3.Database(path.join(import.meta.dirname, "db.sqlite"));
+export const db = new sqlite3.Database(path.join(import.meta.dirname, "db.sqlite"));
 
-  db.serialize(() => {
-    db.run(`
-      CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        firstname TEXT NOT NULL,
-        lastname TEXT NOT NULL,
-        login TEXT NOT NULL,
-        password TEXT NOT NULL,
-        created_at TEXT NOT NULL DEFAULT (datetime('now'))
-      )
-    `);
-  });
-};
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      firstname TEXT NOT NULL,
+      lastname TEXT NOT NULL,
+      login TEXT NOT NULL,
+      password TEXT NOT NULL,
+      created_at TEXT NOT NULL DEFAULT (datetime('now'))
+    )
+  `);
+});
 
-export default configDb;
+
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,30 @@
 import express, { type Request, type Response } from "express";
-import configDb from "./config/db.ts";
-
-const dbRun = configDb();
+import { db } from "./config/db.ts";
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello TypeScript + Express!");
 });
 
 app.post('/auth/signup', (req, res) => {
+  const { firstname, lastname, login, password } = req.body;
 
+  if(!login) return res.status(400).json({error: 'поле title пустое'});
+
+  const sql = 'INSERT INTO users (firstname, lastname, login, password) VALUES (?,?,?,?)';
+
+  db.run(sql, [firstname,lastname,login,password], (err: Error) => {
+    if (err) return res.status(500).json({error: 'не удалось зарегистрировать пользователя'});
+
+    res.status(200).json({
+      message: 'Пользователь зарегистрирован',
+      users: [firstname,lastname,login,password]
+    })
+  })
 })
 
 app.listen(port, () => {
