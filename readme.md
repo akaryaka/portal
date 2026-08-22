99eda1e9f86eadcc012315bba3cb836512998997
andrew <akariaka@yandex.ru>
2026-08-22 19:19:31 +0600

    create path '/me'

---
+++ readme.md
@@ -0,0 +1,4 @@
+server: tests
+
+- conten/type - application/json
+- authorization - bearer token('me')
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,37 +5,46 @@ import { db } from "./config/db.ts";
 
 const app = express();
 const port = 3000;
-
 const JWT_SECRET = 'very-very-very-secret-omg';
 
 app.use(express.json());
+
+function authenticate(req: Request, res: Response, next: any) {
+  const authHeader = req.headers.authorization || '';
+  const token = authHeader.startsWith('Bearer ') ? authHeader.slice(7) : '';
+
+  if (!token) return res.status(401).json({error: 'Нужен токен авторизации'});
+
+  jwt.verify(token, JWT_SECRET, (err: any, user: any) => {
+    if (err) return res.status(401).json({error: 'Токен не действителен или истек'});
+    req.user = user;
+    next();
+  });
+}
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello TypeScript + Express!");
 });
 
-app.post('/auth/signup', (req, res) => {
+app.post('/auth/signup', (req: Request, res: Response) => {
   console.log(req.body);
   
-  // const { firstname, lastname, login, password } = req.body;
-  // const password__hash = bcrypt.hashSync(password, 10);
+  const { firstname, lastname, login, password } = req.body;
+  const password__hash = bcrypt.hashSync(password, 10);
 
-  // const sql = 'INSERT INTO users (firstname, lastname, login, password) VALUES (?,?,?,?)';
+  const sql = 'INSERT INTO users (firstname, lastname, login, password) VALUES (?,?,?,?)';
 
-  // db.run(sql, [firstname,lastname,login,password__hash], (err: Error) => {
-  //   if (err) return res.status(500).json({error: 'не удалось зарегистрировать пользователя'});
+  db.run(sql, [firstname,lastname,login,password__hash], (err: Error) => {
+    if (err) return res.status(500).json({error: 'не удалось зарегистрировать пользователя'});
 
-  //   res.status(200).json({
-  //     message: 'Пользователь зарегистрирован',
-  //     users: [firstname,lastname,login,password__hash]
-  //   })
-  // })
+    res.status(200).json({
+      message: 'Пользователь зарегистрирован',
+      users: [firstname,lastname,login,password__hash]
+    })
+  })
 })
 
-app.post('/auth/login', (req, res) => {
-
-  // console.log(req.body);
-  
+app.post('/auth/login', (req: Request, res: Response) => {
   const { login, password } = req.body;
 
   db.get('SELECT id, login, password FROM users WHERE login = ?', [login], (err, user: any) => {
@@ -55,6 +64,22 @@ app.post('/auth/login', (req, res) => {
     })
   })
 })
+
+app.get('/me', authenticate, (req: Request, res: Response) => {
+  db.get('SELECT id, login, created_at FROM users WHERE id = ?', [req.user.id], (err, user) => {
+    if (err) return res.status(500).json({error: 'Не удалось получить пользователя'});
+    if (!user) return res.status(404).json({error: 'Пользователь не найден'});
+
+    
+    res.json({user})
+  });
+  
+})
+
+app.post('/posts', authenticate, (req, res) => {
+  console.log(req.body);
+  
+})  
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
